90ed193370a2ca63eae3524f7ad5e4b5c0122857
Steven Sussman <sjsussman@gmail.com>
2020-09-21 15:01:05 -0400

    site renders users posts

--- saved-posts/src/components/Saved.js
+++ saved-posts/src/components/Saved.js
@@ -1,37 +1,34 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import SavedCard from './SavedCard'
+// import { useParams } from 'react-router-dom';
 
-const Saved = () =>{
+// const { id } = useParams()
+
+export default function Saved() {
 
     const [saved, setSaved] = useState ([])
 
     useEffect(() => {
-        axios.get('https://reddit-sami.herokuapp.com/api/users/:id/posts')
-
+        axios.get(`https://reddit-sami.herokuapp.com/api/users/1/posts`)
         .then(res =>{
-            console.log(res.data)
-            setSaved(res.data)
+            console.log(res.data.data.posts)
+            setSaved(res.data.data.posts)
         })
-
         .catch(err => {
             console.log(err)
         })
     }, [])
 
-
-    
     return (
         <div> 
             {saved.map(postInfo => {
                 return <SavedCard
-                name={postInfo.name}
-                content={postInfo.content}
+                name={postInfo.post_title}
+                content={postInfo.post_content}
                 />
             })}
         </div>
     )
 
 }
-
-export default Character;
