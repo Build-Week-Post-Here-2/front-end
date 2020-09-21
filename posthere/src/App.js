cb13a87be585c97839a10d930c103a642fc1e267
Sahar Jafari <shr.jafari@gmail.com>
2020-09-21 12:43:44 -0700

    Install bootstrap and setup the SignUp page

--- posthere/src/App.js
+++ posthere/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import SignUp from "./components/Signup";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <SignUp />
+      </div>
     </div>
   );
 }
--- posthere/src/components/Signup.js
+++ posthere/src/components/Signup.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const SignUp = () => {
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <label>
+            Username
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+              value=""
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Email
+            <input
+              type="email"
+              className="form-control"
+              placeholder="email@gmail.com"
+              //   value="email@gmail.com"
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Password
+            <input type="password" className="form-control" value="" />
+          </label>
+        </div>
+        <input type="submit" value="Sign Up" className="btn btn-primary" />
+      </form>
+    </div>
+  );
+};
 
-}
-
-export default SignUp
+export default SignUp;
