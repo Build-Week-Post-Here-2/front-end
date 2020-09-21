6ebedb9671d06359b7aa51db339c1c62e9c7269e
Steven Sussman <sjsussman@gmail.com>
2020-09-21 10:27:38 -0400

    installed yup, created state

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {Route, Switch} from 'react-router-dom'
+import axios from 'axios'
+import './App.css'
+import Login from './Login'
+
+
+const initialFormValues = {
+  username: '',
+  password: '',
+}
+
+const initialFormErrors = {
+  username: '',
+  password: '',
+}
+
+const initialDisabled = true
 
 function App() {
+
+  const [login, setLogin] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useStat(initialDisabled)
+
+
+  const postNewLogin = newLogin => {
+    axios.post('https://reddit-sami.herokuapp.com/api/users/login')
+    .then(res => {
+      setLogin([...login, newLogin])
+      setFormValues(initialFormValues)
+      console.log(newLogin)
+    })
+    .catch(err => {
+      alert('There was an error logging you in, please reload the page and try again.')
+      console.log(err)
+    })
+  }
+  
+  const validate = (name, value) => {
+    yup
+    .reach(schema, name)
+    .validate(value)
+    .then(valid => {
+      setFormErrors({
+        ...formErrors,
+        [name]:''
+      })
+    })
+    .catch(err => {
+      setFormErrors({
+        ...formErrors,
+        [name]: err.errors[0]
+      })
+    })
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    const newOrder = {
+      name: formValues.username.trim(),
+      password: formValues.password.trim(),
+    }
+    postNewLogin(newLogin)
+  }
+
+  useEffect(() => {
+    shema.isValid(formValues)
+    .then(valid => {
+      setDisabled(!valid)
+    })
+  }, [formValues])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
