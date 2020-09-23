6a8b6de60c6f9895c2a7743011614729206f5930
Sahar Jafari <shr.jafari@gmail.com>
2020-09-23 12:10:29 -0700

    save the id and get users posts

--- login-page/src/components/Saved.js
+++ login-page/src/components/Saved.js
@@ -1,37 +1,41 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import SavedCard from './SavedCard'
-import { useParams } from 'react-router-dom';
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import axiosWithAuth from "../utlis/axiosWithAuth";
+import SavedCard from "./SavedCard";
+import { useParams } from "react-router-dom";
 
 export default function Saved() {
+  // const { id } = useParams()
 
-    const { id } = useParams()
+  const [saved, setSaved] = useState([]);
 
-    const [saved, setSaved] = useState ([])
-
-    useEffect(() => {
-        axios
+  useEffect(() => {
+    const id = window.localStorage.getItem("uid");
+    if (id && saved.length === 0) {
+      axiosWithAuth()
         .get(`/users/${id}/posts`)
-        .then(res =>{
-            console.log(res.data.data.posts)
-            setSaved(res.data.data.posts)
+        .then((res) => {
+          //   console.log(res);
+          setSaved(res.data.data.posts);
         })
-        .catch(err => {
-            console.log(err)
-        })
-    }, [])
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [saved]);
 
-    return (
-        <div> 
-            {saved.map(postInfo => {
-                return <SavedCard
-                name={postInfo.post_title}
-                content={postInfo.post_content}
-                />
-            })}
-        </div>
-    )
-
+  return (
+    <div>
+      {saved.map((postInfo, i) => {
+        console.log(postInfo);
+        return (
+          <SavedCard
+            key={i}
+            name={postInfo.post_title}
+            content={postInfo.post_content}
+          />
+        );
+      })}
+    </div>
+  );
 }
