dc07ea37a279f0e03b7f722cd17c69601f1489f1
Sahar Jafari <shr.jafari@gmail.com>
2020-09-23 18:59:04 -0700

    Implement Search

--- login-page/src/components/UserPostSearch.js
+++ login-page/src/components/UserPostSearch.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const UserSearchPost = () => {
+const UserSearchPost = (props) => {
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+    const result = props.posts.filter((post) => {
+      return post["post_title"].includes(searchInput);
+    });
+    setFilteredPosts(result);
+  };
 
-    
-}
+  useEffect(() => {
+    if (filteredPosts.length > 0) {
+      console.log(filteredPosts);
+      props.toggleSearch(filteredPosts);
+    }
+  }, [filteredPosts]);
+  return (
+    <input type="search" value={searchInput} onChange={handleInputChange} />
+  );
+};
 
-export default UserSearchPost
+export default UserSearchPost;
