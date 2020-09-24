98e7c7facc5921b1d45e1115677f8c06c23016f4
Sahar Jafari <shr.jafari@gmail.com>
2020-09-24 10:53:14 -0700

    Fix saving posts in State

--- login-page/src/store/reducers/userReducer.js
+++ login-page/src/store/reducers/userReducer.js
@@ -16,142 +16,144 @@ import {
   ADD_POST_FAIL,
   UPDATE_USER,
   EDIT_POST,
-  DEL_POST_SUCCESS
-} from '../'
+  DEL_POST_SUCCESS,
+} from "../";
 
 const initialState = {
-  data : {
-      userID: "",
-      username: "",
-      password: "",
-      email: "",
-      isLoggedIn: false,
+  data: {
+    userID: "",
+    username: "",
+    password: "",
+    email: "",
+    isLoggedIn: false,
   },
+  posts: [],
   loading: false,
-  error: ""
-}
+  error: "",
+};
 
 export const reducer = (state = initialState, { type, payload }) => {
-
   switch (type) {
-      case SIGN_UP_START: 
-      case LOG_ON_START:
-      case LOAD_START : 
-      case CREATE_POST_START:
-      case ADD_POST_START:
-          return { ...state, loading: true, }
-      case SIGN_UP_SUCCESS:
-          return {
-              user: {
-                  ...state.user,
-                  ...payload,
-                  isLoggedIn: true
-              },
-              loading: false
-          }
-      case LOG_ON_SUCCESS:
-          return { ...state, 
-          user: { username: payload.username,
-              password: payload.password,
-                  isLoggedIn: true }, 
-          loading: false }
-      case CREATE_POST_SUCCESS:
-          console.log(state.posts)
-          return { ...state,
-              user: {
-                  ...state.user,
-                  posts: [ ...state.user.posts, payload], }
+    case SIGN_UP_START:
+    case LOG_ON_START:
+    case LOAD_START:
+    case CREATE_POST_START:
+    case ADD_POST_START:
+      return { ...state, loading: true };
+    case SIGN_UP_SUCCESS:
+      return {
+        user: {
+          ...state.user,
+          ...payload,
+          isLoggedIn: true,
+        },
+        loading: false,
+      };
+    case LOG_ON_SUCCESS:
+      return {
+        ...state,
+        user: {
+          username: payload.username,
+          password: payload.password,
+          isLoggedIn: true,
+        },
+        loading: false,
+      };
+    case CREATE_POST_SUCCESS:
+      console.log(state.posts);
+      return { ...state, posts: [...state.posts, payload] };
+    case ADD_POST_SUCCESS:
+      console.log(payload);
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          posts: state.user.posts.map((list) => {
+            if (list.title === payload.list) {
+              return {
+                ...list,
+                items: [...list.items, payload.todoVals],
+              };
+            } else {
+              return list;
+            }
+          }),
+        },
+      };
+    case DEL_POST_SUCCESS:
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          posts: state.user.posts.map((list) => {
+            if (list.id === payload.id) {
+              const newList = list.items.filter(function (item) {
+                return item.name !== payload.name;
+              });
+              return newList;
+            } else return list;
+          }),
+        },
+      };
+    // case EDIT_POST:
+    //     return {
+    //         ...state,
+    //         user: {
+    //             ...state.user,
+    //             posts: state.user.posts.map( list => {
+    //                 if (list.title === payload.list){
+    //                     return {
+    //                         ...list, items: [ ...list.items, payload.todoVals]
+    //                     }
+    //                 } else {
+    //                     return list
+    //                 }
+    //             })
+    //         }
+    //     }
+    case LOAD_SUCCESS:
+      console.log("LOADING PAYLOAD: ", payload);
+      return {
+        ...state,
+        user: {
+          ...payload,
+        },
+        error: initialState.error,
+        loading: false,
+      };
+    case UPDATE_USER:
+      console.log("UPDATE USER REDUCER", payload);
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...payload,
+        },
+      };
+    case EDIT_POST:
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          posts: state.posts.map((list) => {
+            if (list.todoid === payload.todoid) {
+              for (let todo in list) {
+                if (todo.itemid === payload.itemid) {
+                  todo = payload.todo;
+                } else return todo;
               }
-      case ADD_POST_SUCCESS:
-          console.log(payload)
-          return {
-              ...state,
-              user: {
-                  ...state.user,
-                  posts: state.user.posts.map( list => {
-                      if (list.title === payload.list){
-                          return {
-                              ...list, items: [ ...list.items, payload.todoVals]
-                          }
-                      } else {
-                          return list
-                      }
-                  })
-              }
-          }
-      case DEL_POST_SUCCESS:
-          return {
-              ...state,
-              user: {
-                  ...state.user,
-                  posts: state.user.posts.map( list => {
-                     if ( list.id === payload.id){
-                         const newList = list.items.filter( function(item) {
-                             return item.name !== payload.name
-                         })
-                         return newList
-                     } else return list
-              })
-          }
-      }
-      // case EDIT_POST:
-      //     return {
-      //         ...state,
-      //         user: {
-      //             ...state.user,
-      //             posts: state.user.posts.map( list => {
-      //                 if (list.title === payload.list){
-      //                     return {
-      //                         ...list, items: [ ...list.items, payload.todoVals]
-      //                     }
-      //                 } else {
-      //                     return list
-      //                 }
-      //             })
-      //         }
-      //     }
-      case LOAD_SUCCESS:
-          console.log("LOADING PAYLOAD: ", payload)
-          return { ...state,
-              user: {
-                  ...payload
-              },
-              error: initialState.error,
-              loading: false 
-          }
-      case UPDATE_USER:
-          console.log("UPDATE USER REDUCER", payload);
-    return {
-      ...state,
-      user: {
-                  ...state.user,
-                  ...payload
-      },
-    };
-      case EDIT_POST: 
-          return {
-              ...state,
-              user: {
-                  ...state.user,
-                  posts: state.posts.map( list => {
-                      if ( list.todoid === payload.todoid){
-                          for ( let todo in list ) {
-                              if (todo.itemid === payload.itemid) {
-                                  todo = payload.todo
-                              } else return todo
-                          }
-                          return list
-                      } else return list
-                  })
-              }
-          }
-      case LOG_ON_FAIL:
-      case SIGN_UP_FAIL :
-      case LOAD_FAILURE:
-      case CREATE_POST_FAIL:
-      case ADD_POST_FAIL:
-          return { ...state, error : payload, loading: false };
-      default:
-          return state
-      }
+              return list;
+            } else return list;
+          }),
+        },
+      };
+    case LOG_ON_FAIL:
+    case SIGN_UP_FAIL:
+    case LOAD_FAILURE:
+    case CREATE_POST_FAIL:
+    case ADD_POST_FAIL:
+      return { ...state, error: payload, loading: false };
+    default:
+      return state;
+  }
 };
