8eda58a0084d7bf56f5f1a502ac5c5d7036b33bf
Steven Sussman <sjsussman@gmail.com>
2020-09-24 13:38:37 -0400

    added validation to homepage form

--- login-page/src/components/CreateNewPost.js
+++ login-page/src/components/CreateNewPost.js
@@ -1,6 +1,8 @@
 import React from "react";
+import * as yup from "yup";
+import schema from "../Validation/homeFormSchema";
 import { useDispatch } from "react-redux";
-import { useState, useSelector } from "react";
+import { useState, useSelector, useEffect } from "react";
 import axiosWithAuth from "../utlis/axiosWithAuth";
 import {
   CREATE_POST_START,
@@ -17,9 +19,18 @@ const initFormVals = {
   post_content: "",
 };
 
+const initialFormErrors = {
+  title: "",
+  content: "",
+};
+
 const NewPost = () => {
+  const initialDisabled = true;
   const dispatch = useDispatch();
   const [formVal, setFormVal] = useState(initFormVals);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
   // const user = useSelector(state => state.user)
 
   const handleSubmit = (e) => {
@@ -44,12 +55,37 @@ const NewPost = () => {
       });
   };
   const handleChanges = (e) => {
+
     e.persist();
+    const name = e.target.name;
+    const value = e.target.value
+
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
     setFormVal({
       ...formVal,
-      [e.target.name]: e.target.value,
+      [name]: value,
     });
   };
+
+  useEffect(() => {
+    schema.isValid(formVal).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formVal]);
 
   const classes = useStyles();
 
@@ -57,7 +93,15 @@ const NewPost = () => {
     <div className={classes.container}>
       <form onSubmit={handleSubmit} >
         <div className={classes.root} >
-        <TextField name='post_title' onChange={handleChanges} value={formVal.post_title} id="outlined-basic" label="Post Title Here" variant="outlined" />
+        <TextField 
+        name='post_title' 
+        onChange={handleChanges} 
+        value={formVal.post_title} id="outlined-basic" 
+        label="(Post Title Here)" 
+        variant="outlined" 
+        />
+        <div className={classes.error}>{formErrors.post_title}</div>
+
         <TextField
           id="outlined-multiline-static"
           label="Content"
@@ -67,11 +111,16 @@ const NewPost = () => {
           variant="outlined"
           name="post_content"
           value={formVal.post_content}
-          placeholder="Your post"
+          placeholder="(Post Content Here)"
           onChange={handleChanges}
         />
+        <div className={classes.error}>{formErrors.post_content}</div>
         </div>
-        <CreatePostButton type="submit">Submit</CreatePostButton>
+        <CreatePostButton 
+        type="submit"
+        disabled={disabled}
+        >Submit
+        </CreatePostButton>
     </form>
     </div>
   );
@@ -95,6 +144,10 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: "5px",
     padding: "1%",
     width: "60%",
+  },
+  error: {
+    fontSize: '.4em',
+    color: 'red',
   },
   root: {
     '& > *': {
