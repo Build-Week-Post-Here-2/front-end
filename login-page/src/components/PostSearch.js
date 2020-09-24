f134ab3af12943ccdd9eb0d3d98d70266f1e6ba6
HenryRDavis <rainfallsprings@gmail.com>
2020-09-24 08:25:19 -0500

    doing reccommended search thing

--- login-page/src/components/PostSearch.js
+++ login-page/src/components/PostSearch.js
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 const PostSearch = (props) => {
-  const [postSearch, setSearchInput] = useState("");
-  const [filteredPosts, setFilteredPosts] = useState([]);
-
-  const handleInputChange = (e) => {
-    setSearchInput(e.target.value);
-    const result = props.posts.filter((post) => {
-      return post["post_title"].includes(searchInput);
-    });
-    setFilteredPosts(result);
-  };
 
   useEffect(() => {
 
-});
+  })
+
+  return {
+      
+  }
+};
 
 export default PostSearch;
