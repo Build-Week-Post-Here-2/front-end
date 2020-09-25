e6be95ecbd76fff297da7db3117fe05789cb659e
HenryRDavis <rainfallsprings@gmail.com>
2020-09-24 23:59:35 -0500

    last commit, delete still not working

--- login-page/src/components/EditPost.js
+++ login-page/src/components/EditPost.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles } from "@material-ui/core/styles";
-
 import NewPost from "./CreateNewPost";
 
 const initFormVals = {
