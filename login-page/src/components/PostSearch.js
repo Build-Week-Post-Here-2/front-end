54573a992986f4359288e0d117b518b30708fe38
Steven Sussman <sjsussman@gmail.com>
2020-09-25 11:23:39 -0400

    added styling for search button

--- login-page/src/components/PostSearch.js
+++ login-page/src/components/PostSearch.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 // import axiosWithAuth from "../utlis/axiosWithAuth";
 import axios from 'axios'
 
+import { styled } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
 const initialState = {
         post_title: "",
         post_content: "",
@@ -51,17 +54,29 @@ const PostSearch = (props) => {
         placeholder="Search Title"
         value={searchInput.post_title}
         onChange={inputChanges}
-      />
+      /> <br /> <br /> 
       <input
         name="post_content"
         type="text"
         placeholder="Search Content"
         value={searchInput.post_content}
         onChange={inputChanges}
-      />
-      <button onClick={onSubmit}>Search</button>
+      /> <br />
+      <SearchButton type='submit' onClick={onSubmit}>Search</SearchButton>
       </div>
     );
   }
+
+  const SearchButton = styled(Button)({
+    background: "linear-gradient(280deg, blue 10%, rgb(252,140,3) 90%)",
+    border: 0,
+    borderRadius: 3,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    fontSize: "1.5em",
+    marginTop: "2%",
+  });
+
 
   export default PostSearch;
